92d12df8b2db4398affbb9c51344b7408d255bb4
WorkSamp03 <work.samp03@gmail.com>
2017-05-09 10:46:44 +0000

    test11

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mittens');
-
+//added for testing
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
